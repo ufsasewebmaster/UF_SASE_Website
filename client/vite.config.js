10c9b3390af114e43ad3d819da5468a2e940130c
Ricky Zhang <rickyzhang196@outlook.com>
2024-08-06 19:25:53 -0400

    build: update node.js and npm version for prod

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../server/src/public', // Adjust the output directory to server/src/public
+    outDir: '../server/src/public',
   },
   server: {
     port: 8000,
